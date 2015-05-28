ad4f10cc8bd0deb8cef7d4a58ef91e5ac79167e5
hedgar <hedgar2015@gmail.com>
2015-05-28 18:29:01 +0300

    Homework 1 test

--- erlang/p02.erl
+++
@@ -1,7 +0,0 @@
--module(p02).
--export([but_last/1]).
-
-but_last([Head, SubHead]) ->
-	[Head, SubHead];
-but_last([_ | Tail]) ->
-	but_last(Tail).
--- erlang/p03.erl
+++
@@ -1,9 +0,0 @@
--module(p03).
--export([element_at/2]).
-
-element_at([], _) ->
-	undefined;
-element_at([Head | _], 1) ->
-	Head;
-element_at([_ | Tail], Index) ->
-	element_at(Tail, Index - 1).
