d0dccfc7b4a0e9b6f6358efb12bc0ee0259cd431
hedgar <hedgar2015@gmail.com>
2015-05-31 17:21:01 +0300

    Homework 2 <- bs04: optimization fixes and pattern matching improvement

--- home2/bs04.erl
+++ home2/bs04.erl
@@ -11,18 +11,18 @@ decode_xml(XML) ->
 reassemble(Parts) ->
 	p05:reverse(reassemble(Parts, [])).
 
-%% End of recursive block -> return reached Tail and reversed Acc
-reassemble([{_, close} | Tail], Acc) ->
-	{Tail, p05:reverse(Acc)};
+%% Simple block -> push to Acc and proceed with Tail
+reassemble([{H1, open}, {H2, value}, {_, close} | Tail], Acc) ->
+	reassemble(Tail, [{H1, [], H2} | Acc]);
 
 %% Inner block detected -> dive into and extract the reached Tail, go further with that Tail
 reassemble([{H1, open}, {H2, open} | Tail], Acc) ->
 	{Rest, Result} = reassemble([{H2, open} | Tail], []),
 	reassemble(Rest, [{H1, [], Result} | Acc]);
 
-%% Simple block -> push to Acc and proceed with Tail
-reassemble([{H1, open}, {H2, value}, {_, close} | Tail], Acc) ->
-	reassemble(Tail, [{H1, [], H2} | Acc]);
+%% End of recursive block -> return reached Tail and reversed Acc
+reassemble([{_, close} | Tail], Acc) ->
+	{Tail, p05:reverse(Acc)};
 
 %% Empty list -> return Acc
 reassemble([], Acc) ->
@@ -35,7 +35,7 @@ disassemble(XML) ->
 
 %% XML string -> binary words with a type atom
 disassemble(<<_, _/binary>> = XML, Parts) ->
-	{Rest, Part, Type} = determine(XML, <<>>, open),
+	{Type, Rest, Part} = determine(undef, XML, <<>>),
 	disassemble(Rest, [{Part, Type} | Parts]);
 
 %% End of XML -> return the list of words
@@ -43,26 +43,26 @@ disassemble(<<>>, Parts) ->
 	Parts.
 
 
+%% An ordinar symbol -> proceed
+determine(Type, <<Head, Rest/binary>>, Part) when Head /= $<, Head /= $> ->
+	determine(Type, Rest, <<Part/binary, Head>>);
+
 %% Closing tag detected -> set type atom to 'close'
-determine(<<"</", Rest/binary>>, <<>>, _) ->
-	determine(Rest, <<>>, close);
+determine(undef, <<"</", Rest/binary>>, <<>>) ->
+	determine(close, Rest, <<>>);
 
 %% Opening tag detected -> set type atom to 'open'
-determine(<<"<", Rest/binary>>, <<>>, _) ->
-	determine(Rest, <<>>, open);
+determine(undef, <<"<", Rest/binary>>, <<>>) ->
+	determine(open, Rest, <<>>);
 
 %% End of value field -> return with type 'value'
-determine(<<"<", _/binary>> = XML, Part, _) ->
-	{XML, Part, value};
+determine(undef, <<"<", _/binary>> = XML, Part) ->
+	{value, XML, Part};
 
 %% End of tag -> return
-determine(<<">", Rest/binary>>, Part, Type) ->
-	{Rest, Part, Type};
-
-%% An ordinar symbol -> proceed
-determine(<<Head, Rest/binary>>, Part, Type) ->
-	determine(Rest, <<Part/binary, Head>>, Type);
+determine(Type, <<">", Rest/binary>>, Part) ->
+	{Type, Rest, Part};
 
 %% End of XML -> return
-determine(<<>>, Part, close) ->
-	{<<>>, Part, close}.
+determine(close, <<>>, Part) ->
+	{close, Part, <<>>}.
